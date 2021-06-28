8d068b68d914f58073060006b50cfb97964e631a
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2021-06-28 12:57:54 +0200

    update provider to validate contract

--- provider/src/test/java/es/testacademy/cursos/contract/testing/jvm/demo/provider/ProductPactProviderTest.java
+++ provider/src/test/java/es/testacademy/cursos/contract/testing/jvm/demo/provider/ProductPactProviderTest.java
@@ -10,17 +10,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @Provider("ProductService")
 @PactFolder("pacts")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductPactProviderTest {
-
     @LocalServerPort
     int port;
+
+    @MockBean
+    private ProductRepository productRepository;
 
     @BeforeEach
     void setUp(PactVerificationContext context) {
@@ -35,11 +43,13 @@ public class ProductPactProviderTest {
 
     @State("products exist")
     void toProductsExistState() {
-
+        when(productRepository.fetchAll()).thenReturn(
+                List.of(new Product("09", "CREDIT_CARD", "Gem Visa", "v1"),
+                        new Product("10", "CREDIT_CARD", "28 Degrees", "v1")));
     }
 
     @State("product with ID 10 exists")
     void toProductWithIdTenExistsState() {
-
+        when(productRepository.getById("10")).thenReturn(Optional.of(new Product("10", "CREDIT_CARD", "28 Degrees", "v1")));
     }
 }
